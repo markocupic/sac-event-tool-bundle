c22706d3da98acc850c140cd45a44ef263a63d93
Marko Cupic <m.cupic@gmx.ch>
2018-01-14 13:48:26 +0100

    Development

--- src/Resources/contao/dca/tl_calendar_events_member.php
+++ src/Resources/contao/dca/tl_calendar_events_member.php
@@ -248,7 +248,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events_member'] = array
             'label'     => &$GLOBALS['TL_LANG']['tl_calendar_events_member']['notes'],
             'exclude'   => true,
             'inputType' => 'textarea',
-            'eval'      => array('tl_class' => 'clr', 'mandatory' => true),
+            'eval'      => array('tl_class' => 'clr', 'mandatory' => false),
             'sql'       => "text NULL",
         ),
         'firstname'           => array
