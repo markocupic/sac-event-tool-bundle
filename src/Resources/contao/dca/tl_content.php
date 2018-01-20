cb6bd60a8e1934a6fd2bc6c4856d0f33322707ac
Marko Cupic <m.cupic@gmx.ch>
2018-01-20 16:07:14 +0100

    development

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -18,3 +18,5 @@ if (Input::get('do') == 'sac_calendar_events_tool')
 }
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['ce_user_portrait'] = 'name,type,headline;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+
+
