29616cac6a219997f214e852a7ff966f4dbeedf8
Marko Cupic <m.cupic@gmx.ch>
2018-05-17 12:28:25 +0200

    bugfix wrong image path

--- src/Services/Pdf/WorkshopTCPDF.php
+++ src/Services/Pdf/WorkshopTCPDF.php
@@ -128,7 +128,7 @@ class WorkshopTCPDF extends \TCPDF
 
 
 		// logo
-		$this->Image($rootDir . '/files/sac_pilatus/page_assets/kursbroschuere/logo-sac-2000.png', 150, 5, 50, '', '', '', '', false, 300, '', false, false, 0);
+		$this->Image($rootDir . '/files/fileadmin/page_assets/kursbroschuere/logo-sac-2000.png', 150, 5, 50, '', '', '', '', false, 300, '', false, false, 0);
 
 		// Stripe bottom
 		if ($this->type == 'eventPage')
