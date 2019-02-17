53b3ec14d248256e13cfa91b4b20f53c3505145c
Marko Cupic <m.cupic@gmx.ch>
2019-02-17 17:44:49 +0100

    rename stylesheet

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -21,7 +21,7 @@ require_once($rootDir . '/vendor/markocupic/sac-event-tool-bundle/src/Resources/
 if (TL_MODE == 'BE')
 {
     // Add Backend CSS
-    $GLOBALS['TL_CSS'][] = 'bundles/markocupicsaceventtool/css/stylesheet.css';
+    $GLOBALS['TL_CSS'][] = 'bundles/markocupicsaceventtool/css/be_stylesheet.css';
 }
 
 
