269ca1a23ccd1c17cc11256eb089602d7ab07831
Marko Cupic <m.cupic@gmx.ch>
2019-02-18 09:03:25 +0100

    dev

--- src/ContaoHooks/GetSystemMessages.php
+++ src/ContaoHooks/GetSystemMessages.php
@@ -40,12 +40,12 @@ class GetSystemMessages
             $objUser = BackendUser::getInstance();
             if ($objUser->id > 0)
             {
-                $objEvent = Database::getInstance()->prepare('SELECT * FROM tl_calendar_events WHERE mainInstructor=? AND startDate>? ORDER BY startDate')->execute($objUser->id, time() - 3 * 30 * 24 * 3600);
+                $objEvent = Database::getInstance()->prepare('SELECT * FROM tl_calendar_events WHERE (mainInstructor=? OR registrationGoesTo=?) AND startDate>? ORDER BY startDate')->execute($objUser->id, $objUser->id, time() - 3 * 30 * 24 * 3600);
                 if ($objEvent->numRows)
                 {
                     $strBuffer .= '<h3>' . $GLOBALS['TL_LANG']['MSC']['yourUpcomingEvents'] . '</h3>';
                     $strBuffer .= '<table id="tl_upcoming_events" class="tl_listing">';
-                    $strBuffer .= '<thead><tr><th>Teiln.</th><th>Eventname</th><th></th></tr></thead>';
+                    $strBuffer .= '<thead><tr><th>Teiln.</th><th>Datum &amp; Eventname</th><th></th></tr></thead>';
                     $strBuffer .= '<tbody>';
 
                     $container = System::getContainer();
@@ -56,12 +56,13 @@ class GetSystemMessages
                         $strCSSRowClass = ($objEvent->endDate > time()) ? 'upcoming-event' : 'past-event';
                         $link = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events&id=%s&act=edit&rt=%s', $objEvent->id, $rt);
                         $linkMemberList = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events_member&id=%s&rt=%s', $objEvent->id, $rt);
-                        $strBuffer .= sprintf('<tr class="%s"><td>%s</td><td><a href="%s" title="Event \'%s\' bearbeiten">%s [%s]</a></td><td><a href="%s" title="Zur TN-Liste für \'%s\'">TN-Liste</a></td></tr>',
+                        $strBuffer .= sprintf('<tr class="hover-row %s"><td>%s</td><td>[%s] <a href="%s" style="text-decoration:underline" target="_blank" title="Event \'%s\' bearbeiten">%s</a></td><td><a href="%s" style="text-decoration:underline" target="_blank" title="Zur TN-Liste für \'%s\'">TN-Liste</a></td></tr>',
                             $strCSSRowClass,
                             CalendarEventsHelper::getEventStateOfSubscriptionBadgesString($objEvent),
-                            $link, $objEvent->title,
-                            $objEvent->title,
                             Date::parse(Config::get('dateFormat'), $objEvent->startDate),
+                            $link,
+                            $objEvent->title,
+                            $objEvent->title,
                             $linkMemberList,
                             $objEvent->title
                         );
