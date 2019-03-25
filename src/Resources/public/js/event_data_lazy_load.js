bcb8f9e3dfda2ba97e70ed85ebd1dde92feb9e7a
Marko Cupic <m.cupic@gmx.ch>
2019-03-25 20:06:40 +0100

    dev

--- src/Resources/public/js/event_data_lazy_load.js
+++ src/Resources/public/js/event_data_lazy_load.js
@@ -6,37 +6,43 @@
  * @link https://github.com/markocupic/sac-event-tool-bundle
  */
 (function ($) {
+    /**
+     * Lazyload fields in eventlists after domready
+     */
     $(document).ready(function () {
-        var arrDataXhr = [];
-        var rt = jQuery('[data-request-token]').first().data('request-token');
-        var i=0;
-        jQuery('[data-event-lazyload]').each(function () {
-            i++;
-            var arrData = jQuery(this).data('event-lazyload').split(',');
-            var eventId = arrData[0];
-            var eventField = arrData[1];
-            arrDataXhr.push(arrData);
-            if(i%100 == 0){
-                var arrXHR = arrDataXhr;
-                arrDataXhr = [];
-                if (arrXHR.length) {
-                    var jqxhr = jQuery.post('ajax', {
-                        'REQUEST_TOKEN': rt,
-                        'action': 'getEventData',
-                        'data': JSON.stringify(arrXHR),
-                    }).done(function (json) {
-                        if (json.status === 'success') {
-                            jQuery.each(json.data, function (key, value) {
-                                var element = jQuery('[data-event-lazyload="' + value[0] + ',' + value[1] + '"').first();
-                                jQuery(element).append(value[2]);
-                            });
-                        }
-                        console.log('lazyload' + i);
-                    }).always(function () {
-                        //window.location.reload();
-                    });
+        let arrDataXhr = [];
+        let rt = jQuery('[data-request-token]').first().data('request-token');
+        if (typeof rt === 'undefined') {
+            console.log('Request token is missing.');
+        }
+        let i = 0;
+        let lazyLoadItems = jQuery('[data-event-lazyload]');
+        if (lazyLoadItems.length) {
+            jQuery(lazyLoadItems).each(function () {
+                i++;
+                let arrData = jQuery(this).data('event-lazyload').split(',');
+                arrDataXhr.push(arrData);
+                if (i % 100 === 0 || i === lazyLoadItems.length) {
+                    let arrXHR = arrDataXhr;
+                    arrDataXhr = [];
+                    if (arrXHR.length) {
+                        jQuery.post('ajax', {
+                            'REQUEST_TOKEN': rt,
+                            'action': 'getEventData',
+                            'data': JSON.stringify(arrXHR),
+                        }).done(function (json) {
+                            if (json.status === 'success') {
+                                jQuery.each(json.data, function (key, value) {
+                                    let element = jQuery('[data-event-lazyload="' + value[0] + ',' + value[1] + '"').first();
+                                    jQuery(element).append(value[2]);
+                                });
+                            }
+                        }).always(function () {
+                            //
+                        });
+                    }
                 }
-            }
-        });
+            });
+        }
     });
 })(jQuery);
