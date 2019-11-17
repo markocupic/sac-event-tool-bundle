cf8eeb74592230814a5812b3de801c2a6e4931cd
Marko Cupic <m.cupic@gmx.ch>
2019-11-17 13:03:00 +0100

    optimized the vue eventlist
    Addes "Load mre events" button

--- src/Resources/public/js/eventlist-vue.js
+++ src/Resources/public/js/eventlist-vue.js
@@ -12,20 +12,30 @@ new Vue({
     created: function created() {
         var self = this;
         self.prepareRequest();
-        self.interval = setInterval(function () {
-            self.prepareRequest();
-        }, 200);
+        if (self.enableAutoloading === true) {
+            self.interval = setInterval(function () {
+                self.prepareRequest();
+            }, 200);
+        }
     },
     data: function data() {
         return {
+
+            // If set to false use the button on the bottom of the list to load more items
+            enableAutoloading: false,
+            // Load x items per request
+            itemsPerRequest: 100,
+            // If set to true all items will be loaded at once on the second request
+            loadAllOnSecondRequest: false,
+
+            // Don't touch from here
             eventlist: Eventlist,
             rows: [],
             isBusy: false,
             loadedItems: 0,
             interval: null,
             allEventsLoaded: false,
-            loadAtOnce: 80,
-            loadAllOnSecondRequest: true
+
         };
     },
     methods: {
@@ -36,23 +46,31 @@ new Vue({
             if (self.isBusy === false && self.eventlist.ids.length > self.loadedItems) {
                 self.isBusy = true;
                 self.getDataByXhr();
+                console.log('Loading events...')
             }
 
             if (self.allEventsLoaded === false && self.eventlist.ids.length === self.loadedItems) {
                 self.allEventsLoaded = true;
-                clearInterval(self.interval);
-                console.log('Finished download process. ' + self.loadedItems + ' events loaded.');
+                if (self.enableAutoloading === true && self.interval !== null) {
+
+                    clearInterval(self.interval);
+                }
             }
+
         },
         // Get data by xhr
         getDataByXhr: function getDataByXhr() {
             var self = this;
             var ids = [];
             var counter = 0;
-            var itemsPerRequest = 50;
+            var itemsPerRequest = 100;
+
+            if (self.allEventsLoaded === true) {
+                return;
+            }
 
             if (self.loadedItems === 0) {
-                itemsPerRequest = self.loadAtOnce;
+                itemsPerRequest = self.itemsPerRequest;
             }
 
             if (self.loadedItems > 0 && self.loadAllOnSecondRequest === true) {
@@ -73,22 +91,32 @@ new Vue({
                 'ids': ids,
                 'fields': self.eventlist.fields
             };
+
             var url = self.eventlist.url;
+
             var xhr = $.ajax({
                 type: 'POST',
                 url: url,
                 data: data,
                 dataType: 'json'
             });
+
             xhr.done(function (data) {
                 self.isBusy = false;
                 data.forEach(function (row) {
                     self.rows.push(row);
                     self.loadedItems++;
                 });
+
                 window.setTimeout(function () {
-                    $('#eventList').find('[data-toggle="tooltip"]').tooltip();
+                    $(self.$el).find('[data-toggle="tooltip"]').tooltip();
                 }, 100);
+
+                if (self.allEventsLoaded === false && self.eventlist.ids.length === self.loadedItems) {
+                    self.allEventsLoaded = true;
+                    console.log('Finished downloading process. ' + self.loadedItems + ' events loaded.');
+                }
+
             });
         }
     }
