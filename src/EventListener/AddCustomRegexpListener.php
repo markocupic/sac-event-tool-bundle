c64245d5634301f3d286a87a3d5ead6ce5df57b2
Marko Cupic <m.cupic@gmx.ch>
2019-12-15 14:06:37 +0100

    Update php doc

--- src/EventListener/AddCustomRegexpListener.php
+++ src/EventListener/AddCustomRegexpListener.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 /**
  * SAC Event Tool Web Plugin for Contao
- * Copyright (c) 2008-2019 Marko Cupic
+ * Copyright (c) 2008-2020 Marko Cupic
  * @package sac-event-tool-bundle
- * @author Marko Cupic m.cupic@gmx.ch, 2017-2019
+ * @author Marko Cupic m.cupic@gmx.ch, 2017-2020
  * @link https://github.com/markocupic/sac-event-tool-bundle
  */
 
