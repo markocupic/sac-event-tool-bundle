66c8134039acf956c35b967e03c250c2a34003bc
Marko Cupic <m.cupic@gmx.ch>
2020-02-25 15:05:56 +0100

    Optimized the event story list/reader

--- src/Resources/public/js/eventstory_list_vue.js
+++ src/Resources/public/js/eventstory_list_vue.js
@@ -15,6 +15,8 @@ class EventStory {
             'params': {
                 'listModuleId': null,
                 'readerModuleId': null,
+                'itemIds': [],
+                'perPage': 4,
             },
         };
 
@@ -28,32 +30,55 @@ class EventStory {
                 options: options,
                 listContent: '',
                 readerContent: '',
+                itemIds: [],
+                currentPage: null,
+                currentItemIndex: null,
+                currentItemId: null,
             },
 
             created: function created() {
                 let self = this;
 
+                self.itemIds = self.options.params.itemIds;
+
                 let page = self.getUrlParam('page_e' + self.options.params.listModuleId, null);
-                if (page === null) {
-                    self.prepareRequest('/api/module?id=' + self.options.params.listModuleId);
-                } else {
-                    self.prepareRequest('/api/module?id=' + self.options.params.listModuleId + '&page_e' + self.options.params.listModuleId + '=' + page);
-                }
+                self.currentPage = page === null ? 1 : parseInt(page);
+
+                document.onkeydown = (function (e) {
+                    self._checkKeyPress(e);
+                });
+
+            },
+
+            watch: {
+                currentPage: function (val) {
+                    let self = this;
+                    self.fetchList();
+                },
+                currentItemId: function (val) {
+                    let self = this;
+
+                    // Adjust current page
+                    self.currentItemIndex = self.itemIds.indexOf(parseInt(self.currentItemId));
+                    self.currentPage = Math.floor(parseInt(self.currentItemIndex) / parseInt(self.options.params.perPage)) + 1;
+
+                    // Fetch detail page
+                    self.fetchReaderContent();
+                },
+
             },
 
             methods: {
-                // Prepare ajax request
-                prepareRequest: function prepareRequest(url) {
-                    this.fetchList(url);
-                },
 
                 /**
                  * Fetch list content
                  * Use dieschittigs/contao-content-api-bundle
                  */
-                fetchList: function fetchList(url) {
+                fetchList: function fetchList() {
 
                     let self = this;
+
+                    let url = '/api/module?id=' + self.options.params.listModuleId + '&page_e' + self.options.params.listModuleId + '=' + self.currentPage;
 
                     fetch(url, {
 
@@ -65,7 +90,9 @@ class EventStory {
                     ).then(function (res) {
                         return res.json();
                     }).then(function (json) {
+                        $(elId + ' .event-story-list-container').css('opacity', 0);
                         self.listContent = json.compiledHTML;
+                        $(elId + ' .event-story-list-container').fadeTo('slow', 1);
                     }).then(function () {
                         // trigger same height for item boxes
                         // see: vendor\markocupic\contao-theme-sac-pilatus\src\Resources\contao\files\theme-sac-pilatus\js\theme.js
@@ -76,8 +103,12 @@ class EventStory {
                         $(cssSelectorStr).click(function (e) {
                             e.stopPropagation();
                             e.preventDefault();
+                            let href = $(this).prop('href');
+                            let regexp = new RegExp("page_e" + self.options.params.listModuleId + "=([\\d]+)");
+                            let match = regexp.exec(href);
+                            let page = match ? match[1] : 1;
 
-                            self.fetchList($(this).prop('href'));
+                            self.currentPage = parseInt(page);
                         });
                     }).then(function () {
                         let cssSelectorStr = elId + ' a.event-reader-link';
@@ -102,10 +133,8 @@ class EventStory {
                                 return;
                             }
 
-                            let url = '/api/module?id=' + self.options.params.readerModuleId + '&items=' + itemId;
-
                             // Fetch reader content
-                            self.fetchReaderContent(url);
+                            self.currentItemId = itemId;
                         });
                     });
                 },
@@ -113,10 +142,11 @@ class EventStory {
                 /**
                  * Fetch reader/detail content
                  * Use dieschittigs/contao-content-api-bundle
-                 * @param url
                  */
-                fetchReaderContent: function fetchReaderContent(url) {
+                fetchReaderContent: function fetchReaderContent() {
                     let self = this;
+                    let url = '/api/module?id=' + self.options.params.readerModuleId + '&items=' + self.currentItemId;
+
                     fetch(url, {
                             method: "GET",
                             headers: {
@@ -132,6 +162,49 @@ class EventStory {
                     }).then(function () {
                         self._initLightbox();
                     });
+                },
+
+                /**
+                 * Check for the text item
+                 * @returns {boolean}
+                 */
+                hasNextItem: function hasNextItem() {
+                    let self = this;
+                    if (typeof self.itemIds[self.currentItemIndex + 1] !== 'undefined') {
+                        return true;
+                    }
+                    return false;
+                },
+
+                /**
+                 * Set current ItemId
+                 * The watcher will do the rest...
+                 */
+                goToNextItem: function goToNextItem() {
+                    let self = this;
+                    self.currentItemId = self.itemIds[self.currentItemIndex + 1];
+                },
+
+                /**
+                 * Check for the prev item
+                 * @returns {boolean}
+                 */
+                hasPrevItem: function hasPrevItem() {
+                    let self = this;
+                    if (typeof self.itemIds[self.currentItemIndex - 1] !== 'undefined') {
+                        return true;
+                    }
+                    return false;
+                },
+
+                /**
+                 * Set current ItemId
+                 * The watcher will do the rest...
+                 */
+                goToPrevItem: function goToPrevItem() {
+                    let self = this;
+                    // Fetch reader content
+                    self.currentItemId = self.itemIds[self.currentItemIndex - 1];
                 },
 
                 /**
@@ -169,7 +242,7 @@ class EventStory {
                 },
 
                 /**
-                 *
+                 * Helper method for self.getUrlParam
                  * @private
                  */
                 _getUrlVars: function _getUrlVars() {
@@ -178,6 +251,42 @@ class EventStory {
                         vars[key] = value;
                     });
                     return vars;
+                },
+
+                /**
+                 * Check key press
+                 * @param e
+                 * @private
+                 */
+                _checkKeyPress: function _checkKeyPress(e) {
+
+                    let self = this;
+                    e = e || window.event;
+
+                    // Left arrow
+                    if (e.keyCode == '37') {
+                        if (self.isModalOpen() && self.hasPrevItem()) {
+                            self.goToPrevItem();
+                        }
+                    }
+                    // Right arrow
+                    else if (e.keyCode == '39') {
+                        if (self.isModalOpen() && self.hasNextItem()) {
+                            self.goToNextItem();
+                        }
+                    }
+
+                },
+
+                /**
+                 * Check if modal is open
+                 * @returns {boolean}
+                 */
+                isModalOpen: function isModalOpen() {
+                    if (document.querySelector(elId + ' .modal.show')) {
+                        return true;
+                    }
+                    return false;
                 }
             }
         });
