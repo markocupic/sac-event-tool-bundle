040a54123e04f69d9eafba0fbb7f97f7a95eef88
Marko Cupic <m.cupic@gmx.ch>
2020-07-30 20:04:15 +0200

    bugfix: wrong template name set in annotations of CabanneSacDetailController

--- src/Controller/ContentElement/CabanneSacDetailController.php
+++ src/Controller/ContentElement/CabanneSacDetailController.php
@@ -17,7 +17,6 @@ use Contao\ContentModel;
 use Contao\Controller;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\Framework\ContaoFramework;
-use Contao\Database;
 use Contao\FilesModel;
 use Contao\System;
 use Contao\Template;
@@ -28,10 +27,12 @@ use Contao\CoreBundle\ServiceAnnotation\ContentElement;
 /**
  * Class CabanneSacDetailController
  * @package Markocupic\SacEventToolBundle\Controller\ContentElement
- * @ContentElement("cabanne_sac_detail", category="sac_event_tool_content_elements", template="ce_user_portrait")
+ * @ContentElement("cabanne_sac_detail", category="sac_event_tool_content_elements", template="ce_cabanne_sac_detail")
  */
 class CabanneSacDetailController extends AbstractContentElementController
 {
+    /** @var  CabanneSacModel */
+    protected $objCabanne;
 
     /**
      * @param Request $request
@@ -42,6 +43,15 @@ class CabanneSacDetailController extends AbstractContentElementController
      */
     public function __invoke(Request $request, ContentModel $model, string $section, array $classes = null): Response
     {
+        /** @var CabanneSacModel $cabanneSacModelAdapter */
+        $cabanneSacModelAdapter = $this->get('contao.framework')->getAdapter(CabanneSacModel::class);
+
+        // Add data to template
+        if (null === ($this->objCabanne = $cabanneSacModelAdapter->findByPk($model->cabanneSac)))
+        {
+            return new Response('', Response::HTTP_NO_CONTENT);
+        }
+
         return parent::__invoke($request, $model, $section, $classes);
     }
 
@@ -67,47 +77,37 @@ class CabanneSacDetailController extends AbstractContentElementController
         /** @var  FilesModel $filesModelAdapter */
         $filesModelAdapter = $this->get('contao.framework')->getAdapter(FilesModel::class);
 
-        /** @var  Database $databaseAdapter */
-        $databaseAdapter = $this->get('contao.framework')->getAdapter(Database::class);
-
         /** @var Controller $controllerAdapter */
         $controllerAdapter = $this->get('contao.framework')->getAdapter(Controller::class);
-
-        /** @var CabanneSacModel $cabanneSacModelAdapter */
-        $cabanneSacModelAdapter = $this->get('contao.framework')->getAdapter(CabanneSacModel::class);
 
         $projectDir = System::getContainer()->getParameter('kernel.project_dir');
 
         // Add data to template
-        $objCabanne = $cabanneSacModelAdapter->findByPk($model->cabanneSac);
-        if ($objCabanne !== null)
+        $skip = ['id', 'tstamp'];
+        foreach ($this->objCabanne->row() as $k => $v)
         {
-            $skip = array('id', 'tstamp');
-            foreach ($objCabanne->row() as $k => $v)
+            if (!in_array($k, $skip))
             {
-                if (!in_array($k, $skip))
-                {
-                    $template->$k = $v;
-                }
+                $template->$k = $v;
             }
-            $objFiles = $filesModelAdapter->findByUuid($objCabanne->singleSRC);
+        }
+        $objFiles = $filesModelAdapter->findByUuid($this->objCabanne->singleSRC);
 
-            if ($objFiles !== null && is_file($projectDir . '/' . $objFiles->path))
-            {
-                $model->singleSRC = $objFiles->path;
-                $controllerAdapter->addImageToTemplate($template, $model->row(), null, 'cabanneDetail', $objFiles);
-            }
+        if ($objFiles !== null && is_file($projectDir . '/' . $objFiles->path))
+        {
+            $model->singleSRC = $objFiles->path;
+            $controllerAdapter->addImageToTemplate($template, $model->row(), null, 'cabanneDetail', $objFiles);
+        }
 
-            // coordsCH1903
-            if (!empty($objCabanne->coordsCH1903))
+        // coordsCH1903
+        if (!empty($this->objCabanne->coordsCH1903))
+        {
+            if (strpos($this->objCabanne->coordsCH1903, '/') !== false)
             {
-                if (strpos($objCabanne->coordsCH1903, '/') !== false)
-                {
-                    $template->hasCoords = true;
-                    $arrCoord = explode('/', $objCabanne->coordsCH1903);
-                    $template->coordsCH1903X = trim($arrCoord[0]);
-                    $template->coordsCH1903Y = trim($arrCoord[1]);
-                }
+                $template->hasCoords = true;
+                $arrCoord = explode('/', $this->objCabanne->coordsCH1903);
+                $template->coordsCH1903X = trim($arrCoord[0]);
+                $template->coordsCH1903Y = trim($arrCoord[1]);
             }
         }
 
