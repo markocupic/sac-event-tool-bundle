3547a3d78f7ab005dfed45161910a601287ccd0e
Marko Cupic <m.cupic@gmx.ch>
2022-07-21 20:13:24 +0200

    Refactor the "MemberDashboardDeleteProfileController" (and template)

--- src/Controller/FrontendModule/MemberDashboardDeleteProfileController.php
+++ src/Controller/FrontendModule/MemberDashboardDeleteProfileController.php
@@ -14,19 +14,18 @@ declare(strict_types=1);
 
 namespace Markocupic\SacEventToolBundle\Controller\FrontendModule;
 
-use Contao\Controller;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\Exception\RedirectResponseException;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
 use Contao\Environment;
 use Contao\FrontendUser;
-use Contao\Input;
 use Contao\Message;
 use Contao\ModuleModel;
 use Contao\PageModel;
-use Contao\System;
 use Contao\Template;
 use Haste\Form\Form;
+use Markocupic\SacEventToolBundle\User\FrontendUser\ClearFrontendUserData;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
@@ -39,134 +38,77 @@ class MemberDashboardDeleteProfileController extends AbstractFrontendModuleContr
 {
     public const TYPE = 'member_dashboard_delete_profile';
 
-    /**
-     * @var string
-     */
-    protected $projectDir;
+    private ContaoFramework $framework;
+    private Security $security;
+    private ClearFrontendUserData $clearFrontendUserData;
 
-    /**
-     * @var FrontendUser
-     */
-    protected $objUser;
+    private FrontendUser|null $user;
 
-    /**
-     * @var Template
-     */
-    protected $template;
-
-    /**
-     * @var PageModel
-     */
-    protected $objPage;
+    public function __construct(ContaoFramework $framework, Security $security, ClearFrontendUserData $clearFrontendUserData)
+    {
+        $this->framework = $framework;
+        $this->security = $security;
+        $this->clearFrontendUserData = $clearFrontendUserData;
+    }
 
     public function __invoke(Request $request, ModuleModel $model, string $section, array $classes = null, PageModel $page = null): Response
     {
         // Get logged in member object
-        if (($objUser = $this->get('security.helper')->getUser()) instanceof FrontendUser) {
-            $this->objUser = $objUser;
+        $user = $this->security->getUser();
+
+        if ($user instanceof FrontendUser) {
+            $this->user = $user;
         }
 
         if (null !== $page) {
             // Neither cache nor search page
             $page->noSearch = 1;
             $page->cache = 0;
-
-            // Set the page object
-            $this->objPage = $page;
         }
 
-        $this->projectDir = $this->getParameter('kernel.project_dir');
-
-        // Call the parent method
         return parent::__invoke($request, $model, $section, $classes);
     }
 
-    public static function getSubscribedServices(): array
-    {
-        $services = parent::getSubscribedServices();
-
-        $services['contao.framework'] = ContaoFramework::class;
-        $services['security.helper'] = Security::class;
-
-        return $services;
-    }
-
-    protected function getResponse(Template $template, ModuleModel $model, Request $request): Response|null
+    /**
+     * @throws \Exception
+     */
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
     {
         // Do not allow for not authorized users
-        if (null === $this->objUser) {
+        if (null === $this->user) {
             throw new UnauthorizedHttpException('Not authorized. Please log in as frontend user.');
         }
 
-        // Set adapters
-        $inputAdapter = $this->get('contao.framework')->getAdapter(Input::class);
+        $template->passedConfirmation = false;
+        $template->user = $this->user;
 
-        $this->template = $template;
-
-        $this->template->objUser = $this->objUser;
-
-        if ('clear-profile' === $inputAdapter->get('action')) {
-            // Generate the my profileform
-            $this->template->deleteProfileForm = $this->generateDeleteProfileForm();
-            $this->template->passedConfirmation = true;
+        if ('clear-profile' === $request->query->get('action')) {
+            // Generate the delete profile form
+            $template->deleteProfileForm = $this->generateDeleteProfileForm($request);
+            $template->passedConfirmation = true;
         }
 
         // Add messages to template
-        $this->addMessagesToTemplate();
+        $this->addMessagesToTemplate($request, $template);
 
-        return $this->template->getResponse();
+        return $template->getResponse();
     }
 
     /**
-     * Add messages from session to template.
+     * @throws \Exception
      */
-    protected function addMessagesToTemplate(): void
+    protected function generateDeleteProfileForm(Request $request): string
     {
-        $systemAdapter = $this->get('contao.framework')->getAdapter(System::class);
-        $messageAdapter = $this->get('contao.framework')->getAdapter(Message::class);
-
-        if ($messageAdapter->hasInfo()) {
-            $this->template->hasInfoMessage = true;
-            $session = $systemAdapter->getContainer()->get('session')->getFlashBag()->get('contao.FE.info');
-            $this->template->infoMessage = $session[0];
-        }
-
-        if ($messageAdapter->hasError()) {
-            $this->template->hasErrorMessage = true;
-            $session = $systemAdapter->getContainer()->get('session')->getFlashBag()->get('contao.FE.error');
-            $this->template->errorMessage = $session[0];
-            $this->template->errorMessages = $session;
-        }
-
-        $messageAdapter->reset();
-    }
-
-    /**
-     * @return string|void
-     */
-    protected function generateDeleteProfileForm()
-    {
-        // Set adapters
-        /** @var Input $inputAdapter */
-        $inputAdapter = $this->get('contao.framework')->getAdapter(Input::class);
-
-        /** @var Environment $environmentAdapter */
-        $environmentAdapter = $this->get('contao.framework')->getAdapter(Environment::class);
+        $environmentAdapter = $this->framework->getAdapter(Environment::class);
 
         $objForm = new Form(
             'form-clear-profile',
             'POST',
-            function ($objHaste) {
-                $inputAdapter = $this->get('contao.framework')->getAdapter(Input::class);
-
-                return $inputAdapter->post('FORM_SUBMIT') === $objHaste->getFormId();
-            }
+            static fn ($objHaste) => $request->request->get('FORM_SUBMIT') === $objHaste->getFormId()
         );
 
         $objForm->setFormActionFromUri($environmentAdapter->get('uri'));
 
-        // Now let's add form fields:
-        // Now let's add form fields:
         $objForm->addFormField('deleteProfile', [
             'label' => ['Profil löschen', ''],
             'inputType' => 'select',
@@ -178,41 +120,68 @@ class MemberDashboardDeleteProfileController extends AbstractFrontendModuleContr
             'inputType' => 'text',
         ]);
 
-        // Let's add  a submit button
+        // Let's add a submit button
         $objForm->addFormField('submit', [
             'label' => 'Profil unwiederkehrlich löschen',
             'inputType' => 'submit',
         ]);
 
         if ($objForm->validate()) {
-            if ('form-clear-profile' === $inputAdapter->post('FORM_SUBMIT')) {
-                $blnError = false;
+            if ('form-clear-profile' === $request->request->get('FORM_SUBMIT')) {
+                $blnHasError = false;
 
-                if ('true' !== $inputAdapter->post('deleteProfile')) {
-                    $blnError = true;
+                if ('true' !== $request->request->get('deleteProfile')) {
+                    $blnHasError = true;
                     $objFormField1 = $objForm->getWidget('deleteProfile');
                     $objFormField1->addError('Falsche Eingabe. Das Profil konnte nicht gelöscht werden.');
                 }
 
-                if ($inputAdapter->post('sacMemberId') !== $this->objUser->sacMemberId) {
-                    $blnError = true;
+                if ($request->request->get('sacMemberId') !== (string) $this->user->sacMemberId) {
+                    $blnHasError = true;
                     $objFormField2 = $objForm->getWidget('sacMemberId');
                     $objFormField2->addError('Das Profil konnte nicht gelöscht werden. Die Mitgliedernummer ist falsch.');
                 }
 
-                if (!$blnError) {
-                    // Clear account
-                    $objClearFrontendUserData = System::getContainer()->get('Markocupic\SacEventToolBundle\User\FrontendUser\ClearFrontendUserData');
+                if (!$blnHasError) {
+                    // Clear account and redirect to start page
+                    if (true === $this->clearFrontendUserData->clearMemberProfile((int) $this->user->id)) {
+                        $this->clearFrontendUserData->disableLogin((int) $this->user->id);
+                        $this->clearFrontendUserData->deleteFrontendAccount((int) $this->user->id);
 
-                    if (true === $objClearFrontendUserData->clearMemberProfile((int) $this->objUser->id)) {
-                        $objClearFrontendUserData->disableLogin((int) $this->objUser->id);
-                        $objClearFrontendUserData->deleteFrontendAccount((int) $this->objUser->id);
-                        Controller::redirect('');
+                        throw new RedirectResponseException($request->getSchemeAndHttpHost());
                     }
                 }
             }
         }
 
         return $objForm->generate();
+    }
+
+    /**
+     * Add messages from session to template.
+     */
+    protected function addMessagesToTemplate(Request $request, Template $template): void
+    {
+        $messageAdapter = $this->framework->getAdapter(Message::class);
+
+        $session = $request->getSession();
+        $template->hasInfoMessage = false;
+        $template->hasErrorMessage = false;
+
+        if ($messageAdapter->hasInfo()) {
+            $template->hasInfoMessage = true;
+            $bag = $session->getFlashBag()->get('contao.FE.info');
+            $template->infoMessage = $bag[0];
+            $template->infoMessages = $bag;
+        }
+
+        if ($messageAdapter->hasError()) {
+            $template->hasErrorMessage = true;
+            $bag = $session->getFlashBag()->get('contao.FE.error');
+            $template->errorMessage = $bag[0];
+            $template->errorMessages = $bag;
+        }
+
+        $messageAdapter->reset();
     }
 }
