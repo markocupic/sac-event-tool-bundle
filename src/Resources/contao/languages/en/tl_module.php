bd34738fc8941f3ab80cc01498e39a1687aef978
Marko Cupic <m.cupic@gmx.ch>
2022-07-23 17:28:08 +0200

    Use the constant "TYPE" to define the module name in the contao language files

--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -13,18 +13,18 @@ declare(strict_types=1);
  */
 
 // Legends
-$GLOBALS['TL_LANG']['tl_module']['member_dashboard_upcoming_events_legend'] = 'Member Dashboard: Zukünftige Events';
-$GLOBALS['TL_LANG']['tl_module']['member_dashboard_event_type_filter_legend'] = 'Member Dashboard: Event-Filter-Einstellungen';
 $GLOBALS['TL_LANG']['tl_module']['eventRegloginModule_legend'] = 'Login-Module-Einstellungen';
+$GLOBALS['TL_LANG']['tl_module']['member_dashboard_event_type_filter_legend'] = 'Member Dashboard: Event-Filter-Einstellungen';
+$GLOBALS['TL_LANG']['tl_module']['member_dashboard_upcoming_events_legend'] = 'Member Dashboard: Zukünftige Events';
 
 // Fields
-$GLOBALS['TL_LANG']['tl_module']['unregisterFromEventNotificationId'] = ['Notification Center: Benachrichtigungs-ID bei Eventabsage eines Mitglieds', 'Geben Sie eine Benachrichtigung aus dem Notification Center ein.'];
-$GLOBALS['TL_LANG']['tl_module']['eventType'] = ['Event Art', 'Legen Sie fest welche Event-Arten angezeigt werden sollen.'];
 $GLOBALS['TL_LANG']['tl_module']['eventFilterBoardFields'] = ['Event-Filter-Felder', 'Legen Sie hier die Felder fest, die im Filter angezeigt werden sollen.'];
-$GLOBALS['TL_LANG']['tl_module']['print_export_allowedEventTypes'] = ['Anzuzeigende Event-Typen', 'Geben Sie die anzuzeigenden Event-Typen an.'];
 $GLOBALS['TL_LANG']['tl_module']['eventListLimitPerRequest'] = ['Anzahl zu ladende Events pro Request', 'Legen Sie die Anzahl zu ladende Events pro Request fest. 0 für alle.'];
 $GLOBALS['TL_LANG']['tl_module']['eventListPartialTpl'] = ['Event Auflistungs Kindtemplate', 'Wählen SIe das Kindtemplate (Partialtemplate) aus.'];
-$GLOBALS['TL_LANG']['tl_module']['imageClass'] = ['Bild CSS Klasse', 'Geben Sie eine oder mehrere Bildklassen an. Die Klassen werden dem img tag zugeordnet.'];
-$GLOBALS['TL_LANG']['tl_module']['eventRegLoginModule'] = ['Login Modul', 'Wählen Sie das Login Modul aus, welches im Registrierungs Module verwendet wird.'];
-$GLOBALS['TL_LANG']['tl_module']['eventRegCheckoutLinkPage'] = ['Seite mit Event-Registrierungs-Formular', 'Wählen Sie eine Seite aus..'];
 $GLOBALS['TL_LANG']['tl_module']['eventRegCheckoutLinkLabel'] = ['Zum Event-Registrierungs-Formular Label', 'Schreiben Sie das Link-Label auf.'];
+$GLOBALS['TL_LANG']['tl_module']['eventRegCheckoutLinkPage'] = ['Seite mit Event-Registrierungs-Formular', 'Wählen Sie eine Seite aus..'];
+$GLOBALS['TL_LANG']['tl_module']['eventRegLoginModule'] = ['Login Modul', 'Wählen Sie das Login Modul aus, welches im Registrierungs Module verwendet wird.'];
+$GLOBALS['TL_LANG']['tl_module']['eventType'] = ['Event Art', 'Legen Sie fest welche Event-Arten angezeigt werden sollen.'];
+$GLOBALS['TL_LANG']['tl_module']['imageClass'] = ['Bild CSS Klasse', 'Geben Sie eine oder mehrere Bildklassen an. Die Klassen werden dem img tag zugeordnet.'];
+$GLOBALS['TL_LANG']['tl_module']['print_export_allowedEventTypes'] = ['Anzuzeigende Event-Typen', 'Geben Sie die anzuzeigenden Event-Typen an.'];
+$GLOBALS['TL_LANG']['tl_module']['unregisterFromEventNotificationId'] = ['Notification Center: Benachrichtigungs-ID bei Eventabsage eines Mitglieds', 'Geben Sie eine Benachrichtigung aus dem Notification Center ein.'];
