8e8d7d5ca7dfcf738bfd16edef41188a4f845e1a
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-02-04 12:48:56 +0100

    Use acontroller and a twig template to generate the dasboard on the welcome page in the Contao backend

---
+++ src/Controller/BackendWelcomePage/Dashboard.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SAC Event Tool Bundle.
+ *
+ * (c) Marko Cupic 2023 <m.cupic@gmx.ch>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/sac-event-tool-bundle
+ */
+
+namespace Markocupic\SacEventToolBundle\Controller\BackendWelcomePage;
+
+use Contao\BackendUser;
+use Contao\CalendarEventsModel;
+use Contao\Config;
+use Contao\CoreBundle\Csrf\ContaoCsrfTokenManager;
+use Contao\CoreBundle\Framework\Adapter;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\System;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Markocupic\SacEventToolBundle\CalendarEventsHelper;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Security;
+use Twig\Environment as Twig;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class Dashboard
+{
+    private Adapter $calendarEventsHelperAdapter;
+    private Adapter $configAdapter;
+
+    public function __construct(
+        ContaoFramework $framework,
+        private readonly Connection $connection,
+        private readonly Twig $twig,
+        private readonly Security $security,
+        private readonly ContaoCsrfTokenManager $contaoCsrfTokenManager,
+    ) {
+        // Adapters
+        $this->calendarEventsHelperAdapter = $framework->getAdapter(CalendarEventsHelper::class);
+        $this->configAdapter = $framework->getAdapter(Config::class);
+    }
+
+    /**
+     * @throws Exception
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function generate(): Response
+    {
+        $html = '';
+        /** @var BackendUser $user */
+        $user = $this->security->getUser();
+
+        if ($user instanceof BackendUser) {
+            $html = $this->twig->render(
+                '@MarkocupicSacEventTool/BackendWelcomePage/dashboard.html.twig',
+                [
+                    'upcoming_events' => $this->prepareForTwig($this->getUpcomingEvents($user)),
+                    'past_events' => $this->prepareForTwig($this->getPastEvents($user)),
+                ]
+            );
+        }
+
+        return new Response($html);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getUpcomingEvents(BackendUser $user): array
+    {
+        $timeCut = time() - 15 * 24 * 3600; // 14 + 1 days
+
+        $result = $this->connection->executeQuery(
+            'SELECT * FROM tl_calendar_events AS t1 WHERE (t1.registrationGoesTo = ? OR t1.id IN (SELECT t2.pid FROM tl_calendar_events_instructor AS t2 WHERE t2.userId = ?)) AND t1.startDate > ? ORDER BY t1.startDate',
+            [
+                $user->id,
+                $user->id,
+                $timeCut,
+            ]
+        );
+
+        return $result->fetchAllAssociative();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getPastEvents(BackendUser $user): array
+    {
+        $timeCut = time() - 15 * 24 * 3600; // 14 + 1 days
+
+        $result = $this->connection->executeQuery(
+            'SELECT * FROM tl_calendar_events AS t1 WHERE (t1.registrationGoesTo = ? OR t1.id IN (SELECT t2.pid FROM tl_calendar_events_instructor AS t2 WHERE t2.userId = ?)) AND t1.startDate <= ? AND t1.startDate > ? ORDER BY t1.startDate DESC LIMIT 0,10',
+            [
+                $user->id,
+                $user->id,
+                $timeCut,
+                time() - 1.5 * 365 * 24 * 3600,
+            ]
+        );
+
+        return $result->fetchAllAssociative();
+    }
+
+    private function prepareForTwig(array $arrEvents): array
+    {
+        $events = [];
+        $rt = $this->contaoCsrfTokenManager->getToken(System::getContainer()->getParameter('contao.csrf_token_name'));
+
+        foreach ($arrEvents as $row) {
+            $event = [];
+            $eventModel = CalendarEventsModel::findByPk($row['id']);
+            $linkEvent = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events&id=%s&act=edit&rt=%s', $eventModel->id, $rt);
+            $linkRegistrations = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events_member&id=%s&rt=%s', $eventModel->id, $rt);
+
+            $event['row_class'] = $eventModel->endDate > time() ? 'upcoming-event' : 'past-event';
+            $event['badge'] = $this->calendarEventsHelperAdapter->getEventStateOfSubscriptionBadgesString($eventModel);
+            $event['title'] = $eventModel->title;
+            $event['date'] = date($this->configAdapter->get('dateFormat'), (int) $eventModel->startDate);
+            $event['link_event'] = $linkEvent;
+            $event['link_registrations'] = $linkRegistrations;
+
+            $events[] = $event;
+        }
+
+        return $events;
+    }
+}
--- src/EventListener/Contao/GetSystemMessagesListener.php
+++ src/EventListener/Contao/GetSystemMessagesListener.php
@@ -14,125 +14,39 @@ declare(strict_types=1);
 
 namespace Markocupic\SacEventToolBundle\EventListener\Contao;
 
-use Contao\BackendUser;
-use Contao\CalendarEventsModel;
-use Contao\Config;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
-use Contao\CoreBundle\Framework\ContaoFramework;
-use Contao\Database;
-use Contao\Date;
-use Contao\System;
-use Markocupic\SacEventToolBundle\CalendarEventsHelper;
-use Markocupic\SacEventToolBundle\ContaoScope\ContaoScope;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Doctrine\DBAL\Exception;
+use Markocupic\SacEventToolBundle\Controller\BackendWelcomePage\DashboardController;
+use Symfony\Component\Security\Core\Security;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
- * List all upcoming events (where logged in backend user is the main instructor).
+ * List all upcoming and past events (where logged in backend user is the main instructor).
  */
 #[AsHook('getSystemMessages', priority: 100)]
 class GetSystemMessagesListener
 {
     public function __construct(
-        private readonly ContaoFramework $framework,
-        private readonly ContaoScope $contaoScope,
+        private readonly DashboardController $dashboard,
+        private readonly Security $security,
     ) {
     }
 
+    /**
+     * @throws Exception
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
     public function __invoke(): string
     {
-        $strBuffer = '';
-
-        if ($this->contaoScope->isBackend()) {
-            $backendUserAdapter = $this->framework->getAdapter(BackendUser::class);
-            $databaseAdapter = $this->framework->getAdapter(Database::class);
-            $calendarEventsHelperAdapter = $this->framework->getAdapter(CalendarEventsHelper::class);
-            $dateAdapter = $this->framework->getAdapter(Date::class);
-            $configAdapter = $this->framework->getAdapter(Config::class);
-
-            $objUser = $backendUserAdapter->getInstance();
-
-            $timeCut = time() - 15 * 24 * 3600; // 14 + 1 days
-
-            if ($objUser->id > 0) {
-                // Dashboard: List all upcoming events where user acts as an instructor or where registration goes to the logged-in user.
-                $objEvent = $databaseAdapter->getInstance()
-                    ->prepare('SELECT * FROM tl_calendar_events AS t1 WHERE (t1.registrationGoesTo=? OR t1.id IN (SELECT t2.pid FROM tl_calendar_events_instructor AS t2 WHERE t2.userId=?)) AND t1.startDate>? ORDER BY t1.startDate')
-                    ->execute($objUser->id, $objUser->id, $timeCut)
-                ;
-
-                if ($objEvent->numRows) {
-                    $strBuffer .= '<h3>'.$GLOBALS['TL_LANG']['MSC']['bmd_yourUpcomingEvents'].'</h3>';
-                    $strBuffer .= '<table id="tl_upcoming_events" class="tl_listing">';
-                    $strBuffer .= '<thead><tr><th>Anmeld.</th><th>Datum &amp; Eventname</th><th>Teiln.</th></tr></thead>';
-                    $strBuffer .= '<tbody>';
-
-                    $container = System::getContainer();
-                    $rt = $container->get('contao.csrf.token_manager')->getToken($container->getParameter('contao.csrf_token_name'))->getValue();
-
-                    while ($objEvent->next()) {
-                        $eventModel = CalendarEventsModel::findByPk($objEvent->id);
-
-                        $strCSSRowClass = $objEvent->endDate > time() ? 'upcoming-event' : 'past-event';
-                        $link = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events&id=%s&act=edit&rt=%s', $objEvent->id, $rt);
-                        $linkMemberList = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events_member&id=%s&rt=%s', $objEvent->id, $rt);
-
-                        $strBuffer .= sprintf(
-                            '<tr class="hover-row %s"><td>%s</td><td>[%s] <a href="%s" style="text-decoration:underline" title="Event \'%s\' bearbeiten"><strong>%s</strong></a></td><td><a href="%s" style="text-decoration:underline" title="Zur TN-Liste und Rapporte für \'%s\'">TN-Liste</a></td></tr>',
-                            $strCSSRowClass,
-                            $calendarEventsHelperAdapter->getEventStateOfSubscriptionBadgesString($eventModel),
-                            $dateAdapter->parse($configAdapter->get('dateFormat'), $objEvent->startDate),
-                            $link,
-                            $objEvent->title,
-                            $objEvent->title,
-                            $linkMemberList,
-                            $objEvent->title,
-                        );
-                    }
-                    $strBuffer .= '</tbody>';
-                    $strBuffer .= '</table>';
-                }
-
-                // Dashboard: List past 10 events (or max. 18 months / 1.5 years old) where user acts as an instructor or where registration goes to the logged-in user.
-                $objEvent = $databaseAdapter->getInstance()
-                    ->prepare('SELECT * FROM tl_calendar_events AS t1 WHERE (t1.registrationGoesTo = ? OR t1.id IN (SELECT t2.pid FROM tl_calendar_events_instructor AS t2 WHERE t2.userId = ?)) AND t1.startDate <= ? AND t1.startDate > ? ORDER BY t1.startDate DESC')
-                    ->limit(10)
-                    ->execute($objUser->id, $objUser->id, $timeCut, time() - 1.5 * 365 * 24 * 3600)
-                ;
-
-                if ($objEvent->numRows) {
-                    $strBuffer .= '<h3>'.$GLOBALS['TL_LANG']['MSC']['bmd_yourPastEvents'].'</h3>';
-                    $strBuffer .= '<table id="tl_upcoming_events" class="tl_listing">';
-                    $strBuffer .= '<thead><tr><th>Datum &amp; Eventname</th><th>Rapporte</th></tr></thead>';
-                    $strBuffer .= '<tbody>';
-
-                    $container = System::getContainer();
-                    $rt = $container->get('contao.csrf.token_manager')->getToken($container->getParameter('contao.csrf_token_name'))->getValue();
-
-                    while ($objEvent->next()) {
-                        $strCSSRowClass = $objEvent->endDate > time() ? 'upcoming-event' : 'past-event';
-                        $link = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events&id=%s&act=edit&rt=%s', $objEvent->id, $rt);
-                        $linkMemberList = sprintf('contao/main.php?do=sac_calendar_events_tool&table=tl_calendar_events_member&id=%s&rt=%s', $objEvent->id, $rt);
-
-                        $strBuffer .= sprintf(
-                            '<tr class="hover-row %s"><td>[%s] <a href="%s" style="text-decoration:underline" title="Event \'%s\' bearbeiten"><strong>%s</strong></a></td><td><a href="%s" style="text-decoration:underline" title="Zur TN-Liste und Rapporte für \'%s\'">TN-Liste / Rapporte</a></td></tr>',
-                            $strCSSRowClass,
-                            $dateAdapter->parse($configAdapter->get('dateFormat'), $objEvent->startDate),
-                            $link,
-                            $objEvent->title,
-                            $objEvent->title,
-                            $linkMemberList,
-                            $objEvent->title,
-                        );
-                    }
-                    $strBuffer .= '</tbody>';
-                    $strBuffer .= '</table>';
-                }
-
-                $strBuffer .= '<h3>Anleitungen und Tutorials</h3>';
-                $strBuffer .= '<p>Die Seite "Anleitungen und Tutorials" beim Menü "Service" im Frontend/Website unterstützt Sie bei der Verwendung vom SAC Event-Tool (Backend/Contao).</p>';
-                $strBuffer .= '<hr>';
-            }
+        if ($this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'sac_calendar_events_tool')) {
+            return $this->dashboard->generate()->getContent();
         }
 
-        return $strBuffer;
+        return '';
     }
 }
