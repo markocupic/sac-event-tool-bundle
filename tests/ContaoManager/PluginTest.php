8f4ea23295d4c2c6f65744c29a83b5025d71374b
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-02-26 13:43:34 +0100

    Run ecs for the tests directory

--- tests/ContaoManager/PluginTest.php
+++ tests/ContaoManager/PluginTest.php
@@ -19,38 +19,37 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\DelegatingParser;
 use Contao\TestCase\ContaoTestCase;
-use Markocupic\SacEventToolBundle\ContaoManager\Plugin;
 use Markocupic\RssFeedGeneratorBundle\MarkocupicRssFeedGeneratorBundle;
+use Markocupic\SacEventToolBundle\ContaoManager\Plugin;
 use Markocupic\SacEventToolBundle\MarkocupicSacEventToolBundle;
 
 class PluginTest extends ContaoTestCase
 {
+    public function testInstantiation(): void
+    {
+        $this->assertInstanceOf(Plugin::class, new Plugin());
+    }
 
-	public function testInstantiation(): void
-	{
-		$this->assertInstanceOf(Plugin::class, new Plugin());
-	}
+    public function testGetBundles(): void
+    {
+        $plugin = new Plugin();
 
-	public function testGetBundles(): void
-	{
-		$plugin = new Plugin();
+        /** @var array $bundles */
+        $bundles = $plugin->getBundles(new DelegatingParser());
 
-		/** @var array $bundles */
-		$bundles = $plugin->getBundles(new DelegatingParser());
+        $this->assertCount(2, $bundles);
+        $this->assertInstanceOf(BundleConfig::class, $bundles[0]);
+        $this->assertSame(MarkocupicRssFeedGeneratorBundle::class, $bundles[0]->getName());
 
-		$this->assertCount(2, $bundles);
-		$this->assertInstanceOf(BundleConfig::class, $bundles[0]);
-		$this->assertSame(MarkocupicRssFeedGeneratorBundle::class, $bundles[0]->getName());
-
-		$this->assertInstanceOf(BundleConfig::class, $bundles[1]);
-		$this->assertSame(MarkocupicSacEventToolBundle::class, $bundles[1]->getName());
-		$this->assertSame(
-			[
-				ContaoCalendarBundle::class,
-				ContaoCoreBundle::class,
-				MarkocupicRssFeedGeneratorBundle::class,
-			],
-			$bundles[1]->getLoadAfter()
-		);
-	}
+        $this->assertInstanceOf(BundleConfig::class, $bundles[1]);
+        $this->assertSame(MarkocupicSacEventToolBundle::class, $bundles[1]->getName());
+        $this->assertSame(
+            [
+                ContaoCalendarBundle::class,
+                ContaoCoreBundle::class,
+                MarkocupicRssFeedGeneratorBundle::class,
+            ],
+            $bundles[1]->getLoadAfter()
+        );
+    }
 }
