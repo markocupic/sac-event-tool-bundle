f9dace0380de1e4ffd5fb6e9886aa20d1995e6f4
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-05-22 20:12:25 +0200

    Add the ALL constant.

--- src/Config/EventExecutionState.php
+++ src/Config/EventExecutionState.php
@@ -15,12 +15,18 @@ declare(strict_types=1);
 namespace Markocupic\SacEventToolBundle\Config;
 
 /**
- * These states are used for the tour report.
+ * These states are used for tour reports.
  */
 class EventExecutionState
 {
     public const STATE_EXECUTED_LIKE_PREDICTED = 'event_executed_like_predicted';
+    public const STATE_DEFERRED = 'event_deferred';
     public const STATE_ADAPTED = 'event_adapted';
     public const STATE_CANCELED = 'event_canceled';
-    public const STATE_DEFERRED = 'event_deferred';
+    public const ALL = [
+        self::STATE_EXECUTED_LIKE_PREDICTED,
+        self::STATE_DEFERRED,
+        self::STATE_ADAPTED,
+        self::STATE_CANCELED,
+    ];
 }
