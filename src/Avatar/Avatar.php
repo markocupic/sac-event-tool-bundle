d47e0efe458727b9ec3a0e906974904437e3e9fb
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-05-22 20:12:02 +0200

    Refactor work

--- src/Avatar/Avatar.php
+++ src/Avatar/Avatar.php
@@ -14,26 +14,32 @@ declare(strict_types=1);
 
 namespace Markocupic\SacEventToolBundle\Avatar;
 
+use Contao\CoreBundle\Framework\Adapter;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\FilesModel;
 use Contao\MemberModel;
 use Contao\UserModel;
 
 class Avatar
 {
+    private Adapter $filesModelAdapter;
+
     public function __construct(
+        private readonly ContaoFramework $framework,
         private readonly string $projectDir,
         private readonly string $sacevtAvatarFemale,
         private readonly string $sacevtAvatarMale,
     ) {
+        $this->filesModelAdapter = $this->framework->getAdapter(FilesModel::class);
     }
 
     /**
-     * @todo serve an avatar if gender === 'other'
+     * @todo Provide an avatar if gender === 'other'
      */
     public function getAvatarResourcePath(MemberModel|UserModel $userModel): string
     {
         if (!empty($userModel->avatar)) {
-            $objFiles = FilesModel::findByUuid($userModel->avatar);
+            $objFiles = $this->filesModelAdapter->findByUuid($userModel->avatar);
 
             if (null !== $objFiles) {
                 if (is_file($this->projectDir.'/'.$objFiles->path)) {
@@ -45,13 +51,6 @@ class Avatar
         if ('female' === $userModel->gender) {
             return $this->sacevtAvatarFemale;
         }
-
-        if ('male' === $userModel->gender) {
-            return $this->sacevtAvatarMale;
-        }
-        //elseif ('other' === $userModel->gender) {
-        //return $this->sacevtAvatarOther;
-        //}
 
         return $this->sacevtAvatarMale;
     }
