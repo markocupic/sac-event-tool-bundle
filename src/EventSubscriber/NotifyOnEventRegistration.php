03af322db3f1f53c0c498317bef628cd87c2c42a
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-05-22 21:46:52 +0200

    Use getter methods from EventRegistrationEvent to obtain registration data

--- src/EventSubscriber/NotifyOnEventRegistration.php
+++ src/EventSubscriber/NotifyOnEventRegistration.php
@@ -15,9 +15,8 @@ declare(strict_types=1);
 namespace Markocupic\SacEventToolBundle\EventSubscriber;
 
 use Contao\CalendarEventsModel;
+use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
-use Contao\Date;
-use Contao\Environment;
 use Contao\Events;
 use Contao\MemberModel;
 use Contao\ModuleModel;
@@ -32,16 +31,25 @@ final class NotifyOnEventRegistration implements EventSubscriberInterface
 {
     public const PRIORITY = 10000;
 
+    private Adapter $calendarEventsHelperAdapter;
+    private Adapter $eventsAdapter;
+    private Adapter $notificationAdapter;
+    private Adapter $userModelAdapter;
+
     private array $arrData = [];
-    private ContaoFramework|null $framework = null;
     private MemberModel|null $memberModel = null;
     private CalendarEventsModel|null $eventModel = null;
     private CalendarEventsMemberModel|null $eventMemberModel = null;
     private ModuleModel|null $moduleModel = null;
 
     public function __construct(
+        private readonly ContaoFramework $framework,
         private readonly string $sacevtLocale,
     ) {
+        $this->calendarEventsHelperAdapter = $this->framework->getAdapter(CalendarEventsHelper::class);
+        $this->eventsAdapter = $this->framework->getAdapter(Events::class);
+        $this->notificationAdapter = $this->framework->getAdapter(Notification::class);
+        $this->userModelAdapter = $this->framework->getAdapter(UserModel::class);
     }
 
     public static function getSubscribedEvents(): array
@@ -55,39 +63,20 @@ final class NotifyOnEventRegistration implements EventSubscriberInterface
     {
         $this->initialize($event);
 
-        // Set adapters
-        /** @var UserModel $userModelAdapter */
-        $userModelAdapter = $this->framework->getAdapter(UserModel::class);
-
-        /** @var Date $dateAdapter */
-        $dateAdapter = $this->framework->getAdapter(Date::class);
-
-        /** @var CalendarEventsHelper $calendarEventsHelperAdapter */
-        $calendarEventsHelperAdapter = $this->framework->getAdapter(CalendarEventsHelper::class);
-
-        /** @var Environment $environmentAdapter */
-        $environmentAdapter = $this->framework->getAdapter(Environment::class);
-
-        /** @var Events $eventsAdapter */
-        $eventsAdapter = $this->framework->getAdapter(Events::class);
-
-        /** @var Notification $notificationAdapter */
-        $notificationAdapter = $this->framework->getAdapter(Notification::class);
-
         /** @var Notification $objNotification */
-        $objNotification = $notificationAdapter->findByPk($this->moduleModel->receiptEventRegistrationNotificationId);
-
-        // Switch sender/recipient if the main instructor has delegated event registrations administration work to somebody else
-        $bypassRegistration = false;
+        $objNotification = $this->notificationAdapter->findByPk($this->moduleModel->receiptEventRegistrationNotificationId);
 
         /** @var UserModel $objInstructor */
-        $objInstructor = $userModelAdapter->findByPk($this->eventModel->mainInstructor);
+        $objInstructor = $this->userModelAdapter->findByPk($this->eventModel->mainInstructor);
 
         $strRegistrationGoesToName = '';
         $strRegistrationGoesToEmail = '';
 
+        // Switch sender/recipient if the main instructor has delegated event registrations administration work to somebody else
+        $bypassRegistration = false;
+
         if ($this->eventModel->registrationGoesTo) {
-            $objUser = $userModelAdapter->findByPk($this->eventModel->registrationGoesTo);
+            $objUser = $this->userModelAdapter->findByPk($this->eventModel->registrationGoesTo);
 
             if (null !== $objUser) {
                 if ('' !== $objUser->email) {
@@ -115,14 +104,14 @@ final class NotifyOnEventRegistration implements EventSubscriberInterface
                 'event_iban' => $this->eventModel->addIban ? html_entity_decode((string) $this->eventModel->iban) : '',
                 'event_ibanBeneficiary' => $this->eventModel->addIban ? html_entity_decode((string) $this->eventModel->ibanBeneficiary) : '',
                 'event_id' => $this->eventModel->id,
-                'event_link_detail' => 'https://'.$environmentAdapter->get('host').'/'.$eventsAdapter->generateEventUrl($this->eventModel),
+                'event_link_detail' => $this->eventsAdapter->generateEventUrl($this->eventModel, true),
                 'event_name' => html_entity_decode($eventType.$this->eventModel->title),
                 'instructor_email' => $bypassRegistration ? html_entity_decode((string) $strRegistrationGoesToEmail) : html_entity_decode($objInstructor->email),
                 'instructor_name' => $bypassRegistration ? html_entity_decode((string) $strRegistrationGoesToName) : html_entity_decode($objInstructor->name),
                 'participant_ahv_number' => html_entity_decode((string) ($this->arrData['ahvNumber'] ?? '')),
                 'participant_city' => html_entity_decode($this->memberModel->city),
                 'participant_contao_member_id' => $this->memberModel->id,
-                'participant_date_of_birth' => ($this->arrData['dateOfBirth'] ?? 0) > 0 ? $dateAdapter->parse('d.m.Y', $this->arrData['dateOfBirth']) : '---',
+                'participant_date_of_birth' => ($this->arrData['dateOfBirth'] ?? 0) > 0 ? date('d.m.Y', (int) $this->arrData['dateOfBirth']) : '---',
                 'participant_email' => $this->memberModel->email !== $this->arrData['email'] ? $this->arrData['email'] : $this->memberModel->email,
                 'participant_emergency_phone' => $this->arrData['emergencyPhone'],
                 'participant_emergency_phone_name' => html_entity_decode((string) ($this->arrData['emergencyPhoneName'] ?? '')),
@@ -133,7 +122,7 @@ final class NotifyOnEventRegistration implements EventSubscriberInterface
                 'participant_notes' => html_entity_decode((string) ($this->arrData['notes'] ?? '')),
                 'participant_postal' => $this->memberModel->postal,
                 'participant_sac_member_id' => $this->memberModel->sacMemberId,
-                'participant_section_membership' => $calendarEventsHelperAdapter->getSectionMembershipAsString($this->memberModel),
+                'participant_section_membership' => $this->calendarEventsHelperAdapter->getSectionMembershipAsString($this->memberModel),
                 'participant_state_of_subscription' => html_entity_decode((string) $GLOBALS['TL_LANG']['MSC'][$this->eventMemberModel->stateOfSubscription]),
                 'participant_street' => html_entity_decode($this->memberModel->street),
             ];
@@ -144,12 +133,11 @@ final class NotifyOnEventRegistration implements EventSubscriberInterface
 
     private function initialize(EventRegistrationEvent $event): void
     {
-        $this->framework = $event->framework;
-        $this->arrData = $event->arrData;
-        $this->memberModel = $event->memberModel;
-        $this->eventModel = $event->eventModel;
-        $this->eventMemberModel = $event->eventMemberModel;
-        $this->moduleModel = $event->moduleModel;
+        $this->arrData = $event->getData();
+        $this->memberModel = $event->getContaoMemberModel();
+        $this->eventModel = $event->getEvent();
+        $this->eventMemberModel = $event->getRegistration();
+        $this->moduleModel = $event->getRegistrationModule();
 
         if (!$this->framework->isInitialized()) {
             $this->framework->initialize(true);
