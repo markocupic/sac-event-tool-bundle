9d693a9660fca43bae0d6b6b11c44e90ea7b86f1
Jonas Mueller <jonasmueller1@outlook.com>
2023-10-01 17:24:16 +0200

    feat: add link to change release level

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -116,6 +116,7 @@ $GLOBALS['TL_LANG']['MSC']['bhs_dashb_yourUpcomingEvents'] = 'Ihre nächsten Eve
 $GLOBALS['TL_LANG']['MSC']['bhs_dashb_yourPastEvents'] = 'Ihre vergangenen Events';
 $GLOBALS['TL_LANG']['MSC']['bhs_dashb_howToEditReadonlyProfileData'] = 'Änderungen an Name, Adresse, Telefon und E-Mail müssen auf der Webseite des SAC Zentralverbandes (https://sac-cas.ch) gemacht werden.';
 $GLOBALS['TL_LANG']['MSC']['bhs_dashb_editEvent'] = 'Event bearbeiten';
+$GLOBALS['TL_LANG']['MSC']['bhs_dashb_editReleaseLevel'] = 'Freigabestufe ändern';
 $GLOBALS['TL_LANG']['MSC']['bhs_dashb_eventListing'] = 'Event-Liste';
 $GLOBALS['TL_LANG']['MSC']['bhs_dashb_livePreview'] = 'Event Vorschau';
 $GLOBALS['TL_LANG']['MSC']['bhs_dashb_printReport'] = 'Tourrapport drucken';
