adebf8da03e114fce9fe1f351c347f7c7bd8ba1f
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2024-01-25 23:12:21 +0100

    Feat: Inject the backend welcome page dasboard after the system messages
    https://github.com/jonasmueller1/sac-pilatus-website/issues/114#issuecomment-1910289992

--- src/EventListener/Contao/ParseTemplateListener.php
+++ src/EventListener/Contao/ParseTemplateListener.php
@@ -18,15 +18,10 @@ use Contao\CoreBundle\Controller\BackendController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\Template;
-use Doctrine\DBAL\Exception;
 use Markocupic\SacEventToolBundle\Controller\BackendHomeScreen\DashboardController;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Security;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 
-#[AsHook('parseTemplate', priority: 100)]
 class ParseTemplateListener
 {
     public function __construct(
@@ -36,26 +31,33 @@ class ParseTemplateListener
     ) {
     }
 
-    /**
-     * @throws Exception
-     * @throws LoaderError
-     * @throws RuntimeError
-     * @throws SyntaxError
-     */
-    public function __invoke(Template $template): void
+    #[AsHook('parseTemplate', priority: 100)]
+    public function addDashboardToTheWelcomePage(Template $template): void
     {
         // List upcoming and past events on the backend home screen.
         $request = $this->requestStack->getCurrentRequest();
 
         // Do not show the dashboard when using custom routes/controllers
-        if ($request->attributes->get('_controller') === BackendController::class.'::mainAction') {
-            if (str_starts_with($template->getName(), 'be_main')) {
-                if (!$request->query->has('mtg') && !$request->query->has('error') && !$request->query->has('do') && !$request->query->has('act')) {
-                    if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'sac_calendar_events_tool')) {
-                        $template->main = $this->dashboard->generate()->getContent().$template->main;
-                    }
-                }
-            }
+        if ($request->attributes->get('_controller') !== BackendController::class.'::mainAction') {
+            return;
         }
+
+        if (!str_starts_with($template->getName(), 'be_main')) {
+            return;
+        }
+
+        if ($request->query->has('mtg') || $request->query->has('error') || $request->query->has('do') || $request->query->has('act')) {
+            return;
+        }
+
+        if (!$this->security->isGranted('ROLE_ADMIN') && !$this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'sac_calendar_events_tool')) {
+            return;
+        }
+
+        // Generate the dashboard
+        $dashboardMarkup = $this->dashboard->generate()->getContent();
+
+        // Inject the dashboard before the shortcuts, but after system messages
+        $template->main = str_replace('<div id="tl_shortcuts">', $dashboardMarkup.'<div id="tl_shortcuts">', $template->main);
     }
 }
