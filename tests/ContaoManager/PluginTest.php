df4bbec343d4b8980a5a3f8b853a973a95a26938
Marko Cupic <m.cupic@gmx.ch>
2024-02-06 18:00:00 +0100

    Fix PluginTest

--- tests/ContaoManager/PluginTest.php
+++ tests/ContaoManager/PluginTest.php
@@ -55,7 +55,7 @@ class PluginTest extends ContaoTestCase
                 ContaoCoreBundle::class,
                 MarkocupicRssFeedGeneratorBundle::class,
             ],
-            $bundles[1]->getLoadAfter()
+            $bundles[2]->getLoadAfter()
         );
     }
 }
