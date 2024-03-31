7b485f88ce695879a6b6c8b690c99ee3d7e3b8bc
Marko Cupic <m.cupic@gmx.ch>
2024-03-31 22:21:05 +0200

    Upgrade notifications on event publish and on change event release level

--- src/EventListener/ChangeEventReleaseLevelListener.php
+++ src/EventListener/ChangeEventReleaseLevelListener.php
@@ -15,15 +15,22 @@ declare(strict_types=1);
 namespace Markocupic\SacEventToolBundle\EventListener;
 
 use Contao\BackendUser;
+use Contao\CalendarEventsModel;
 use Contao\Config;
 use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\Email;
+use Contao\StringUtil;
+use Contao\UserModel;
+use Markocupic\SacEventToolBundle\CalendarEventsHelper;
 use Markocupic\SacEventToolBundle\Event\ChangeEventReleaseLevelEvent;
 use Markocupic\SacEventToolBundle\Model\EventReleaseLevelPolicyModel;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
 
 #[AsEventListener]
 final readonly class ChangeEventReleaseLevelListener
@@ -35,12 +42,21 @@ final readonly class ChangeEventReleaseLevelListener
         private ContaoFramework $framework,
         private ScopeMatcher $scopeMatcher,
         private Security $security,
+        private Environment $twig,
+        private RouterInterface $router,
     ) {
         $this->framework->initialize();
         $this->configAdapter = $this->framework->getAdapter(Config::class);
         $this->eventReleaseLevelPolicyModelAdapter = $this->framework->getAdapter(EventReleaseLevelPolicyModel::class);
     }
 
+	/**
+	 * @param ChangeEventReleaseLevelEvent $event
+	 * @return void
+	 * @throws \Twig\Error\LoaderError
+	 * @throws \Twig\Error\RuntimeError
+	 * @throws \Twig\Error\SyntaxError
+	 */
     public function __invoke(ChangeEventReleaseLevelEvent $event): void
     {
         $strDirection = $event->getDirection();
@@ -75,15 +91,59 @@ final readonly class ChangeEventReleaseLevelListener
             $objEmail = new Email();
             $objEmail->from = $this->configAdapter->get('adminEmail');
             $objEmail->fromName = 'Administrator SAC Pilatus';
-            $objEmail->subject = sprintf('Neue Freigabestufe (%s) für Event %s.', $objEventReleaseLevel->level, $objEvent->title);
+            $objEmail->replyTo('noreply@sac-pilatus.ch');
 
-            if ('down' === $strDirection) {
-                $objEmail->text = sprintf("Hallo \nEvent %s wurde soeben durch %s auf Freigabestufe %s (%s) hinuntergestuft. \n\n\nDies ist eine automatische Nachricht mit Ursprung in: %s\n Liebe Grüsse \n\n Administrator SAC Pilatus", $objEvent->title, $user->name, $objEventReleaseLevel->level, $objEventReleaseLevel->title, __METHOD__.' LINE: '.__LINE__);
-            } else {
-                $objEmail->text = sprintf("Hallo \nEvent %s wurde soeben durch %s auf Freigabestufe %s (%s) hochgestuft. \n\n\nDies ist eine automatische Nachricht mit Ursprung in: %s\n Liebe Grüsse \n\n Administrator SAC Pilatus", $objEvent->title, $user->name, $objEventReleaseLevel->level, $objEventReleaseLevel->title, __METHOD__.' LINE: '.__LINE__);
-            }
+            $objEmail->subject = sprintf('Neue Freigabestufe (%s) für Event %s.', $objEventReleaseLevel->level, StringUtil::revertInputEncoding($objEvent->title));
+            $objEmail->text = $this->parseEmailText($objEvent, UserModel::findByPk($user->id), $strDirection);
 
             $objEmail->sendTo($recipients);
         }
+    }
+
+	/**
+	 * @param CalendarEventsModel $objEvent
+	 * @param UserModel $objUser
+	 * @param string $strDirection
+	 * @return string
+	 * @throws \Twig\Error\LoaderError
+	 * @throws \Twig\Error\RuntimeError
+	 * @throws \Twig\Error\SyntaxError
+	 */
+    private function parseEmailText(CalendarEventsModel $objEvent, UserModel $objUser, string $strDirection): string
+    {
+        $eventReleaseLevel = EventReleaseLevelPolicyModel::findByPk($objEvent->eventReleaseLevel);
+
+        if (null === $eventReleaseLevel) {
+            throw new \RuntimeException(sprintf('Could not find a event release level for event with ID %d.', $objEvent->id));
+        }
+
+        $arrEvent = array_map(static fn ($val) => StringUtil::revertInputEncoding((string) $val), $objEvent->row());
+
+        $objInstructor = CalendarEventsHelper::getMainInstructor($objEvent);
+
+        if (null === $objInstructor) {
+            throw new \RuntimeException(sprintf('Could not find a main instructor for event with ID %d.', $objEvent->id));
+        }
+
+        return $this->twig->render(
+            '@MarkocupicSacEventTool/NotifyOnEventReleaseLevelChange/notify_on_event_release_level_change.twig',
+            [
+                'user' => $objUser->row(),
+                'instructor' => $objInstructor->row(),
+                'event' => $arrEvent,
+                'event_release_level' => $eventReleaseLevel->row(),
+                'action' => 'up' === $strDirection ? 'hochgestuft' : 'hinuntergestuft',
+                'event_link' => $this->router->generate(
+                    'contao_backend',
+                    [
+                        'do' => 'calendar',
+                        'table' => CalendarEventsModel::getTable(),
+                        'id' => $objEvent->id,
+                        'act' => 'edit',
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL,
+                ),
+            ]
+        );
     }
 }
--- src/EventListener/PublishEventListener.php
+++ src/EventListener/PublishEventListener.php
@@ -15,14 +15,25 @@ declare(strict_types=1);
 namespace Markocupic\SacEventToolBundle\EventListener;
 
 use Contao\BackendUser;
+use Contao\CalendarEventsModel;
 use Contao\Config;
 use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\Email;
+use Contao\StringUtil;
+use Contao\UserModel;
+use Markocupic\SacEventToolBundle\CalendarEventsHelper;
 use Markocupic\SacEventToolBundle\Event\PublishEventEvent;
+use Markocupic\SacEventToolBundle\Model\EventReleaseLevelPolicyModel;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 #[AsEventListener]
 final readonly class PublishEventListener
@@ -33,11 +44,18 @@ final readonly class PublishEventListener
         private ContaoFramework $framework,
         private ScopeMatcher $scopeMatcher,
         private Security $security,
+        private Environment $twig,
+        private RouterInterface $router,
     ) {
         $this->framework->initialize();
         $this->configAdapter = $this->framework->getAdapter(Config::class);
     }
 
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
     public function __invoke(PublishEventEvent $event): void
     {
         $request = $event->getRequest();
@@ -59,16 +77,56 @@ final readonly class PublishEventListener
                     $objEmail = new Email();
                     $objEmail->from = $this->configAdapter->get('adminEmail');
                     $objEmail->fromName = 'Administrator SAC Pilatus';
-                    $objEmail->subject = sprintf('Event %s wurde veröffentlicht', $objCalendarEvent->title);
+                    $objEmail->replyTo('noreply@sac-pilatus.ch');
 
-                    $strText = "Hallo \nEvent %s wurde soeben durch %s veröffentlicht. \n\n";
-                    $strText .= "Dies ist eine automatische Nachricht mit Ursprung in: %s\n Liebe Grüsse\n\nAdministrator SAC Pilatus";
-
-                    $objEmail->text = sprintf($strText, $objCalendarEvent->title, $user->name, __METHOD__.' LINE: '.__LINE__);
+                    $objEmail->subject = sprintf('Event %s wurde veröffentlicht', StringUtil::revertInputEncoding($objCalendarEvent->title));
+                    $objEmail->text = $this->parseEmailText($objCalendarEvent, UserModel::findByPk($user->id));
 
                     $objEmail->sendTo($objCalendar->notifyOnEventReleaseLevelChange);
                 }
             }
         }
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    private function parseEmailText(CalendarEventsModel $objEvent, UserModel $objUser): string
+    {
+        $eventReleaseLevel = EventReleaseLevelPolicyModel::findByPk($objEvent->eventReleaseLevel);
+
+        if (null === $eventReleaseLevel) {
+            throw new \RuntimeException(sprintf('Could not find a event release level for event with ID %d.', $objEvent->id));
+        }
+
+        $arrEvent = array_map(static fn ($val) => StringUtil::revertInputEncoding((string) $val), $objEvent->row());
+
+        $objInstructor = CalendarEventsHelper::getMainInstructor($objEvent);
+
+        if (null === $objInstructor) {
+            throw new \RuntimeException(sprintf('Could not find a main instructor for event with ID %d.', $objEvent->id));
+        }
+
+        return $this->twig->render(
+            '@MarkocupicSacEventTool/NotifyOnEventReleaseLevelChange/notify_on_event_publish.twig',
+            [
+                'user' => $objUser->row(),
+                'instructor' => $objInstructor->row(),
+                'event' => $arrEvent,
+                'event_release_level' => $eventReleaseLevel->row(),
+                'event_link' => $this->router->generate(
+                    'contao_backend',
+                    [
+                        'do' => 'calendar',
+                        'table' => CalendarEventsModel::getTable(),
+                        'id' => $objEvent->id,
+                        'act' => 'edit',
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL,
+                ),
+            ]
+        );
     }
 }
