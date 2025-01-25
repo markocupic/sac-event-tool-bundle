564b71976449fc84d8656c512990dc08580b7659
Marko Cupic <m.cupic@gmx.ch>
2025-01-25 17:54:34 +0100

    Add the "substr" function to TWIG

--- src/Twig/Extension/TwigStringUtilManager.php
+++ src/Twig/Extension/TwigStringUtilManager.php
@@ -14,10 +14,12 @@ declare(strict_types=1);
 
 namespace Markocupic\SacEventToolBundle\Twig\Extension;
 
+use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\StringUtil;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class TwigStringUtilManager extends AbstractExtension
 {
@@ -33,10 +35,25 @@ class TwigStringUtilManager extends AbstractExtension
         ];
     }
 
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('substr', [$this, 'substr']),
+        ];
+    }
+
     public function revertInputEncoding(string $str): string
     {
-        $stringUtil = $this->framework->getAdapter(StringUtil::class);
+        return $this->getStringUtil()->revertInputEncoding($str);
+    }
 
-        return $stringUtil->revertInputEncoding($str);
+    public function substr($strString, $intNumberOfChars, string $strEllipsis = ' …'): string
+    {
+        return $this->getStringUtil()->substr($strString, $intNumberOfChars, $strEllipsis);
+    }
+
+    private function getStringUtil(): Adapter
+    {
+        return $this->framework->getAdapter(StringUtil::class);
     }
 }
