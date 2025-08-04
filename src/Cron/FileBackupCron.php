9c1c87a56c246cdff72c50612da57fdd0d007f45
Marko Cupic <m.cupic@gmx.ch>
2025-08-05 00:03:48 +0200

    Implement a new Cron service to back up sensitive directories

--- src/Cron/FileBackupCron.php
+++ src/Cron/FileBackupCron.php
@@ -15,73 +15,131 @@ declare(strict_types=1);
 namespace Markocupic\SacEventToolBundle\Cron;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
-use Doctrine\DBAL\Connection;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Filesystem\Path;
 use Markocupic\ZipBundle\Zip\Zip;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Component\Finder\Finder;
 
+/**
+ * This class handles file backup operations for different intervals including
+ * daily, weekly, and monthly. It schedules cron jobs for creating backups and
+ * manages old backups by removing files older than a specified time threshold.
+ */
 class FileBackupCron extends AbstractController
 {
+    private const string DATIM_FORMAT = 'Y_m_d_H_i_s';
 
-	public function __construct(
-		private readonly Connection $connection,
-		private readonly string     $projectDir,
-	)
-	{
-	}
+    private const array BACKUP_DIRS = [
+        'files/fileadmin',
+        'files/sektion',
+    ];
 
-	#[AsCronJob('15 13 * * *')]
-	public function backupFiles1(): void
-	{
-		$dir1 = Path::join($this->projectDir, 'files/fileadmin');
-		$dir2 = Path::join($this->projectDir, 'files/sektion');
+    public function __construct(
+        #[Autowire('%router.request_context.host%')]
+        private readonly string $hostname,
+        private readonly string $projectDir,
+    ) {
+    }
 
-		$strip = Path::join($this->projectDir, 'files');
-		$destinaton = Path::join($this->projectDir, '/../backups/sacpilatus_files_page_assets_' . date('Ymd_His') . '.zip');
+    /**
+     * Every first day of the month at 1:45 AM.
+     */
+    #[AsCronJob('45 1 5 * *')]
+    public function monthly(): void
+    {
+        $timeCut = strtotime('-4 month');
+        $this->removeOutdatedBackups('monthly', $timeCut);
+        $this->backupFiles('monthly');
+    }
 
-		(new Zip())
-			->ignoreDotFiles(false)
-			->stripSourcePath($strip)
-			->addDirRecursive($dir1)
-			->addDirRecursive($dir2)
-			->run($destinaton);
-	}
+    /**
+     * Every Sunday at 2:45 AM.
+     */
+    #[AsCronJob('45 2 * * 7')]
+    public function weekly(): void
+    {
+        $timeCut = strtotime('-1 month');
+        $this->removeOutdatedBackups('weekly', $timeCut);
+        $this->backupFiles('weekly');
+    }
 
-	#[AsCronJob('0 13 * * *')]
-	public function backupFiles2(): void
-	{
-		$dir = Path::join($this->projectDir, 'files/sektion');
-		$strip = Path::join($this->projectDir, 'files');
-		$destinaton = Path::join($this->projectDir, '/../backups/sacpilatus_files_sektion_' . date('Ymd_His') . '.zip');
+    /**
+     * Every day at 3:45 AM.
+     */
+    #[AsCronJob('45 3 * * *')]
+    public function daily(): void
+    {
+        $timeCut = strtotime('-1 hour');
+        $this->removeOutdatedBackups('daily', $timeCut);
+        $this->backupFiles('daily');
+    }
 
-		(new Zip())
-			->ignoreDotFiles(false)
-			->stripSourcePath($strip)
-			->addDirRecursive($dir)
-			->run($destinaton);
-	}
+    public function backupFiles(string $interval): void
+    {
+        $arrDirs = array_filter(
+            array_map(fn ($dir) => Path::join($this->projectDir, $dir), self::BACKUP_DIRS),
+            static fn ($fullPath) => file_exists($fullPath),
+        );
 
-	public function restore(): void
-	{
-		throw new \Exception('The app is currently disabled');
+        if (empty($arrDirs)) {
+            return;
+        }
 
-		$backAll = $this->connection->fetchAllAssociative('SELECT * FROM tl_files_bak WHERE path LIKE "files/fileadmin/page_assets%"');
+        $filename = \sprintf('files___%s.zip', date(self::DATIM_FORMAT));
+        $targetDir = $this->buildBackupDir($interval);
 
-		foreach ($backAll as $back) {
-			$res = $this->connection->fetchOne('SELECT id FROM tl_files WHERE path = ?', [$back['path']]);
+        $destPath = Path::join($targetDir, $filename);
 
-			if ($res > 0) {
-				$set = [
-					'uuid' => $back['uuid'],
-					'hash' => $back['hash'],
-					'name' => $back['name'],
-				];
-				$this->connection->update('tl_files', $set, ['id' => $res]);
-			} else {
-				$set = $back;
-				unset($set['id']);
-				$this->connection->insert('tl_files', $set);
-			}
-		}
-	}
+        (new Filesystem())->mkdir($targetDir);
+
+        $zip = (new Zip())
+            ->ignoreDotFiles(false)
+            ->stripSourcePath($this->projectDir)
+        ;
+
+        foreach ($arrDirs as $dir) {
+            $zip->addDirRecursive($dir);
+        }
+
+        $zip->run($destPath);
+    }
+
+    private function buildBackupDir(string $interval): string
+    {
+        $hostname = preg_replace('/[^a-zA-Z0-9]/', '_', $this->hostname);
+
+        return Path::join($this->projectDir, \sprintf('/../backups/%s/%s', $hostname, $interval));
+    }
+
+    private function removeOutdatedBackups(string $interval, int $timeCut): void
+    {
+        $arrFiles = $this->fetchFiles($this->buildBackupDir($interval));
+
+        foreach ($arrFiles as $splFileInfo) {
+            $this->removeFileByTimestampCutoff($splFileInfo, $timeCut);
+        }
+    }
+
+    /**
+     * @return array<\SplFileInfo>
+     */
+    private function fetchFiles(string $dir): array
+    {
+        $finder = new Finder();
+
+        return iterator_to_array($finder->files()->in($dir));
+    }
+
+    private function removeFileByTimestampCutoff(\SplFileInfo $splFileInfo, int $timeCut): void
+    {
+        $chunks = explode('___', pathinfo($splFileInfo->getFilename(), PATHINFO_FILENAME));
+        $dateString = $chunks[1];
+        $objDate = \DateTime::createFromFormat(self::DATIM_FORMAT, $dateString);
+
+        if ($objDate->getTimestamp() < $timeCut) {
+            (new Filesystem())->remove($splFileInfo->getPathname());
+        }
+    }
 }
