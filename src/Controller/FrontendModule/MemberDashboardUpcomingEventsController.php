7f8af86ca97150a6fa0fdb151861ac1fa96e3d8e
Marko Cupic <m.cupic@gmx.ch>
2025-10-02 10:12:11 +0200

    Refactor the member dashboard upcoming events frontend module

--- src/Controller/FrontendModule/MemberDashboardUpcomingEventsController.php
+++ src/Controller/FrontendModule/MemberDashboardUpcomingEventsController.php
@@ -18,7 +18,6 @@ use Codefog\HasteBundle\UrlParser;
 use Contao\Controller;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
-use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\FrontendUser;
 use Contao\Message;
@@ -26,26 +25,23 @@ use Contao\ModuleModel;
 use Contao\PageModel;
 use Contao\Validator;
 use Markocupic\SacEventToolBundle\Model\CalendarEventsMemberModel;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\UriSigner;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
-#[AsFrontendModule(MemberDashboardUpcomingEventsController::TYPE, category: 'sac_event_tool_frontend_modules', template: 'mod_member_dashboard_upcoming_events')]
+#[AsFrontendModule(MemberDashboardUpcomingEventsController::TYPE, category: 'sac_event_tool_frontend_modules')]
 class MemberDashboardUpcomingEventsController extends AbstractFrontendModuleController
 {
     public const string TYPE = 'member_dashboard_upcoming_events';
 
     private FrontendUser|null $user = null;
 
-    private FragmentTemplate|null $template = null;
-
     public function __construct(
-        private readonly ContaoFramework $framework,
         private readonly RequestStack $requestStack,
-        private readonly Security $security,
+        private readonly TokenStorageInterface $tokenStorage,
         private readonly UriSigner $uriSigner,
         private readonly UrlParser $urlParser,
     ) {
@@ -53,9 +49,7 @@ class MemberDashboardUpcomingEventsController extends AbstractFrontendModuleCont
 
     public function __invoke(Request $request, ModuleModel $model, string $section, array|null $classes = null, PageModel|null $page = null): Response
     {
-        if (($objUser = $this->security->getUser()) instanceof FrontendUser) {
-            $this->user = $objUser;
-        }
+        $this->user = $this->getUserFromToken();
 
         if (null !== $page) {
             // Neither cache nor search page
@@ -71,31 +65,45 @@ class MemberDashboardUpcomingEventsController extends AbstractFrontendModuleCont
     {
         // Do not allow for not authorized users
         if (null === $this->user) {
-            throw new UnauthorizedHttpException('Not authorized. Please log in as frontend user.');
+            throw new UnauthorizedHttpException('Not authorized. Please log in as a frontend user.');
         }
-
-        $this->template = $template;
-
-        // Set adapters
-        $messageAdapter = $this->framework->getAdapter(Message::class);
-        $validatorAdapter = $this->framework->getAdapter(Validator::class);
-        $calendarEventsMemberModelAdapter = $this->framework->getAdapter(CalendarEventsMemberModel::class);
-        $controllerAdapter = $this->framework->getAdapter(Controller::class);
 
         // Handle messages
-        if (empty($this->user->email) || !$validatorAdapter->isEmail($this->user->email)) {
-            $messageAdapter->addInfo('Leider wurde für dieses Konto in der Datenbank keine E-Mail-Adresse gefunden. Daher stehen einige Funktionen nur eingeschränkt zur Verfügung. Bitte hinterlegen Sie auf der Internetseite des Zentralverbands Ihre E-Mail-Adresse.');
+        if (empty($this->user->email) || !$this->getContaoAdapter(Validator::class)->isEmail($this->user->email)) {
+            $this->getContaoAdapter(Message::class)->addInfo('Leider wurde für dieses Konto in der Datenbank keine E-Mail-Adresse gefunden. Daher stehen einige Funktionen nur eingeschränkt zur Verfügung. Bitte hinterlege auf der Internetseite des Zentralverbands deine E-Mail-Adresse.');
         }
 
-        // Add messages to template
-        $this->addMessagesToTemplate($request);
+        // Add messages to the template
+        $this->addMessagesToTemplate($template, $request);
 
         // Load language
-        $controllerAdapter->loadLanguageFile('tl_calendar_events_member');
+        $this->getContaoAdapter(Controller::class)->loadLanguageFile('tl_calendar_events_member');
 
         // Upcoming events
-        $arrUpcoming = $calendarEventsMemberModelAdapter->findUpcomingEventsByMemberId($this->user->id);
-        $arrUpcoming = array_map(
+        $template->set('arrUpcomingEvents', $this->fetchUpcomingEvents($model));
+
+        return $template->getResponse();
+    }
+
+    private function getUserFromToken(): FrontendUser|null
+    {
+        $user = $this->tokenStorage
+            ->getToken()
+            ?->getUser()
+        ;
+
+        if ($user instanceof FrontendUser) {
+            return $user;
+        }
+
+        return null;
+    }
+
+    private function fetchUpcomingEvents(ModuleModel $model): array
+    {
+        $arrUpcoming = $this->getContaoAdapter(CalendarEventsMemberModel::class)->findUpcomingEventsByMemberId($this->user->id);
+
+        return array_map(
             function ($row) use ($model) {
                 $row['deregistrationUrl'] = $this->generateDeregistrationUrl($row['eventRegistrationModel'], $model);
 
@@ -103,47 +111,44 @@ class MemberDashboardUpcomingEventsController extends AbstractFrontendModuleCont
             },
             $arrUpcoming,
         );
-        $this->template->set('arrUpcomingEvents', $arrUpcoming);
-
-        return $this->template->getResponse();
     }
 
     private function generateDeregistrationUrl(CalendarEventsMemberModel $registrationModel, ModuleModel $moduleModel): string
     {
-        $objPage = $this->framework->getAdapter(PageModel::class)->findById($moduleModel->eventDeregistrationPage);
+        $page = $this->getContaoAdapter(PageModel::class)->findById($moduleModel->eventDeregistrationPage);
 
-        if (null === $objPage) {
+        if (null === $page) {
             return '';
         }
 
-        $queryString = \sprintf('regId=%d&callbackUrl=%s', $registrationModel->id, $this->requestStack->getCurrentRequest()->getUri());
+        $query = \sprintf('regId=%d&callbackUrl=%s', $registrationModel->id, $this->requestStack->getCurrentRequest()->getUri());
 
-        $url = $this->urlParser->addQueryString($queryString, $objPage->getFrontendUrl());
+        $url = $this->urlParser->addQueryString($query, $page->getFrontendUrl());
 
         return $this->uriSigner->sign($url);
     }
 
     /**
-     * Add messages from session flash to template.
+     * Add messages from session flash to the template.
      */
-    private function addMessagesToTemplate(Request $request): void
+    private function addMessagesToTemplate(FragmentTemplate $template, Request $request): void
     {
-        $messageAdapter = $this->framework->getAdapter(Message::class);
+        $messageAdapter = $this->getContaoAdapter(Message::class);
 
-        $this->template->set('hasInfoMessage', false);
-        $this->template->set('hasErrorMessage', false);
+        $template->set('hasInfoMessage', false);
+        $template->set('hasErrorMessage', false);
 
         if ($messageAdapter->hasInfo()) {
             $session = $request->getSession()->getFlashBag()->get('contao.FE.info');
-            $this->template->set('hasInfoMessage', true);
-            $this->template->set('infoMessage', $session[0]);
+            $template->set('hasInfoMessage', true);
+            $template->set('infoMessage', $session[0]);
         }
 
         if ($messageAdapter->hasError()) {
             $session = $request->getSession()->getFlashBag()->get('contao.FE.error');
-            $this->template->set('hasErrorMessage', true);
-            $this->template->set('errorMessage', $session[0]);
-            $this->template->set('errorMessages', $session);
+            $template->set('hasErrorMessage', true);
+            $template->set('errorMessage', $session[0]);
+            $template->set('errorMessages', $session);
         }
 
         $messageAdapter->reset();
