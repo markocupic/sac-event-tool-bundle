9ef83faf9af1d85f57880bd628f75d56ed9c9346
Marko Cupic <m.cupic@gmx.ch>
2025-11-02 00:24:16 +0100

    Add new Twig functions 'has_backend_account' and 'current_user_backend_account'

--- src/Twig/Extension/TwigLoggedInFrontendUserManager.php
+++ src/Twig/Extension/TwigLoggedInFrontendUserManager.php
@@ -18,18 +18,23 @@ use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
 use Contao\MemberModel;
+use Contao\UserModel;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class TwigLoggedInFrontendUserManager extends AbstractExtension
 {
-    private Adapter $member;
+    private Adapter $memberAdapter;
+
+    private Adapter $userAdapter;
 
     public function __construct(
         private readonly ContaoFramework $framework,
         private readonly TokenChecker $tokenChecker,
     ) {
-        $this->member = $this->framework->getAdapter(MemberModel::class);
+        $this->framework->initialize();
+        $this->memberAdapter = $this->framework->getAdapter(MemberModel::class);
+        $this->userAdapter = $this->framework->getAdapter(UserModel::class);
     }
 
     public function getFunctions(): array
@@ -37,6 +42,8 @@ class TwigLoggedInFrontendUserManager extends AbstractExtension
         return [
             new TwigFunction('has_logged_in_frontend_user', [$this, 'hasLoggedInFrontendUser']),
             new TwigFunction('get_logged_in_frontend_user', [$this, 'getLoggedInFrontendUser']),
+            new TwigFunction('has_backend_account', [$this, 'hasBackendAccount']),
+            new TwigFunction('current_user_backend_account', [$this, 'getCurrentUserBackendAccount']),
         ];
     }
 
@@ -65,11 +72,65 @@ class TwigLoggedInFrontendUserManager extends AbstractExtension
     public function getLoggedInFrontendUser(): MemberModel|null
     {
         if ($this->tokenChecker->hasFrontendUser()) {
-            if (null !== ($model = $this->member->findByUsername($this->tokenChecker->getFrontendUsername()))) {
+            if (null !== ($model = $this->memberAdapter->findByUsername($this->tokenChecker->getFrontendUsername()))) {
                 return $model;
             }
         }
 
         return null;
+    }
+
+    /**
+     * Checks if the current user has an associated backend account.
+     *
+     *  Inside your Twig template: {% if has_backend_account %}<a href="/contao">Backend</a>{% endif %}
+     *
+     * @return bool true if a backend account exists, false otherwise
+     */
+    public function hasBackendAccount(): bool
+    {
+        return null !== $this->getCurrentUserBackendAccount();
+    }
+
+    /**
+     * Retrieves the backend account of the currently logged-in frontend user, if any.
+     *
+     * This method checks whether there is a frontend user logged in. If a frontend
+     * user exists, it attempts to find the corresponding backend user account
+     * associated with the frontend user's ID. Additional checks are performed to
+     * ensure the backend account is active and not disabled.
+     *
+     * Returns null if no frontend user is logged in, no corresponding backend user is
+     * found, or the backend user is disabled.
+     *
+     * Inside your Twig template: {% set backend_user = current_user_backend_account() %}
+     *
+     * @return UserModel|null the backend user account model or null if no valid account is found
+     */
+    public function getCurrentUserBackendAccount(): UserModel|null
+    {
+        if (!$this->tokenChecker->hasFrontendUser()) {
+            return null;
+        }
+
+        $frontendUser = $this->getLoggedInFrontendUser();
+
+        if (null === $frontendUser) {
+            return null;
+        }
+
+        $backendUser = $this->userAdapter->findOneBySacMemberId($frontendUser->username);
+
+        if (null === $backendUser) {
+            return null;
+        }
+
+        $disabled = ('' !== $backendUser->start && $backendUser->start > time()) || ('' !== $backendUser->stop && $backendUser->stop <= time());
+
+        if ($disabled || $backendUser->disable) {
+            return null;
+        }
+
+        return $backendUser;
     }
 }
