6a5ccec66198c98fbb457ee6b3f4a9c60faa9961
Marko Cupic <m.cupic@gmx.ch>
2025-11-02 11:26:49 +0100

    Update ECS config file

--- tools/ecs/config/default.php
+++ tools/ecs/config/default.php
@@ -4,32 +4,31 @@ declare(strict_types=1);
 
 use Contao\EasyCodingStandard\Set\SetList;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
-use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return ECSConfig::configure()
-    ->withSets([SetList::CONTAO])
-    ->withPaths([
-        __DIR__ . '/../../src',
-    ])
-    ->withSkip([
-		MethodChainingIndentationFixer::class => [
-            '*/DependencyInjection/Configuration.php',
+	->withSets([SetList::CONTAO])
+	->withPaths([
+		__DIR__ . '/../../src',
+	])
+	->withSkip([
+		\Contao\EasyCodingStandard\Fixer\CommentLengthFixer::class          => ['*.php'],
+		\PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer::class  => [
+			'*/DependencyInjection/Configuration.php',
 		],
-        UnusedVariableSniff::class => [
-            //'core-bundle/tests/Session/Attribute/ArrayAttributeBagTest.php',
+		\SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff::class => [
+			//'core-bundle/tests/Session/Attribute/ArrayAttributeBagTest.php',
 			'*/Controller/Cloudconvert/Sandbox/WebhookController.php',
 			'*/Pdf/WorkshopTCPDF.php',
 		],
 		__DIR__ . '/../../src/Controller/Cloudconvert/Sandbox/WebhookController.php',
 
 	])
-    ->withRootFiles()
-    ->withParallel()
-    ->withSpacing(Option::INDENTATION_SPACES, "\n")
-    ->withConfiguredRule(HeaderCommentFixer::class, [
+	->withRootFiles()
+	->withParallel()
+	->withSpacing(Option::INDENTATION_SPACES, "\n")
+	->withConfiguredRule(HeaderCommentFixer::class, [
 		'header' => "This file is part of SAC Event Tool Bundle.\n\n(c) Marko Cupic <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/sac-event-tool-bundle",
-    ])
-    ->withCache(sys_get_temp_dir() . '/ecs/markocupic/sac-event-tool-bundle');
+	])
+	->withCache(sys_get_temp_dir() . '/ecs/markocupic/sac-event-tool-bundle');
