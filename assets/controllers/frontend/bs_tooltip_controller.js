11b884f5cff5462d4df5744aea1639e85a9e5809
Marko Cupic <m.cupic@gmx.ch>
2026-03-21 22:14:18 +0100

    Default tooltip title to empty string
    If no data-bs-tooltip-title is present, default the Tooltip title to an empty string (using ?? '') to avoid passing undefined to Bootstrap. Also normalize the placement fallback to 'top' (single-quote style).

--- assets/controllers/frontend/bs_tooltip_controller.js
+++ assets/controllers/frontend/bs_tooltip_controller.js
@@ -4,10 +4,10 @@ import { Tooltip } from "bootstrap";
 export default class extends Controller {
     connect() {
         this.tooltip = new Tooltip(this.element, {
-            title: this.element.dataset.bsTooltipTitle,
+            title: this.element.dataset.bsTooltipTitle ?? '',
             html: true,
             trigger: "hover",
-            placement: this.element.dataset.bsTooltipPlacement ?? "top",
+            placement: this.element.dataset.bsTooltipPlacement ?? 'top',
         });
     }
 
